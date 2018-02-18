965c9618873056e165fba71720a5f1cc8d9dbcc1
Travis Haley <travis@haleycomputersolutions.com>
2018-02-18 10:33:30 -0700

    port again

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(80, err => {
+  createServer(handler).listen(5000, err => {
     if (err) throw err;
-    console.log('Ready on localhost:80');
+    console.log('Ready on localhost:5000');
   });
 });
